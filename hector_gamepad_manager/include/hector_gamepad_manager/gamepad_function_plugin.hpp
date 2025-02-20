d9184a1149d72800f3826564a11075edd8b0b4b5
Aljoscha Schmidt <57407113+Joschi3@users.noreply.github.com>
2025-02-20 15:35:10 +0100

    Feature/multi configs (#3)
    * Added buttons for axes
    
    * Switch Active Configuration
    
    * load configs from different pkg
    
    * refactored member variables and allow loading configs from different package
    
    * Refactored drive plugin
    
    * Use relative topics for cmd_vel and joy
    
    * Refactored member variables in Gamepad Manager
    
    * Set Robot Name at Startup
    
    * Always publish StampedTwist & parametrized cmd vel topic
    
    * use sub nodes and relative paths in plugins
    
    * Renamed Parameters and anonymous node name
    
    * Renamed file_name to config_name
    
    ---------
    
    Co-authored-by: Simon Giegerich <simon.giegerich@stud.tu-darmstadt.de>
    Co-authored-by: Aljoscha Schmidt <aljoscha.schmidt@tu-darmstadt.de>
    Co-authored-by: Martin Oehler <oehler@sim.tu-darmstadt.de>

--- hector_gamepad_manager/include/hector_gamepad_manager/gamepad_function_plugin.hpp
+++ hector_gamepad_manager/include/hector_gamepad_manager/gamepad_function_plugin.hpp
@@ -15,9 +15,8 @@ public:
    * @brief Initialize function that is called when the plugin is loaded.
    *
    * @param node The ROS node that the plugin is associated with.
-   * @param active True if the plugin should be active on startup, false otherwise.
    */
-  virtual void initialize( const rclcpp::Node::SharedPtr &node, const bool active ) = 0;
+  virtual void initialize( const rclcpp::Node::SharedPtr &node ) = 0;
 
   /**
    * @brief Handle button input events.
@@ -25,7 +24,26 @@ public:
    * @param function The function name that is associated with the button.
    * @param pressed True if the button is pressed, false otherwise.
    */
-  virtual void handleButton( const std::string &function, const bool pressed ) = 0;
+  virtual void handleButton( const std::string &function, const bool pressed )
+  {
+    if ( button_states_.count( function ) == 0 ) {
+      button_states_[function] = false;
+    }
+
+    if ( pressed ) {
+      if ( button_states_[function] ) {
+        handleHold( function );
+      } else {
+        handlePress( function );
+      }
+    } else {
+      if ( button_states_[function] ) {
+        handleRelease( function );
+      }
+    }
+
+    button_states_[function] = pressed;
+  }
 
   /**
    * @brief Handle axis input events.
@@ -33,7 +51,32 @@ public:
    * @param function The function name that is associated with the axis.
    * @param value The value of the axis input event.
    */
-  virtual void handleAxis( const std::string &function, const double value ) = 0;
+  virtual void handleAxis( const std::string &function, double value )
+  {
+    (void)function;
+    (void)value;
+  }
+
+  /**
+   * @brief Function for handling input events once if the button or axis is pressed.
+   *
+   * @param function The name of the function that is associated with the input event.
+   */
+  virtual void handlePress( const std::string &function ) { (void)function; }
+
+  /**
+   * @brief Function for handling input events where the button or axis is held down.
+   *
+   * @param function The name of the function that is associated with the input event.
+   */
+  virtual void handleHold( const std::string &function ) { (void)function; }
+
+  /**
+   * @brief Function for handling input events where the button or axis is released.
+   *
+   * @param function The name of the function that is associated with the input event.
+   */
+  virtual void handleRelease( const std::string &function ) { (void)function; }
 
   /**
    * @brief Update function that is called periodically to update the plugin state after handling input events.
@@ -50,12 +93,24 @@ public:
    */
   virtual void deactivate() = 0;
 
+  /**
+   * @brief Check if the plugin is active.
+   * @return True if the plugin is active, false otherwise.
+   */
+  bool isActive() const { return active_; }
+
 protected:
+  // The ROS node
+  rclcpp::Node::SharedPtr node_;
+
   // The namespace of the plugin.
   std::string plugin_namespace_;
 
   // Specifies if the plugin is active.
   bool active_ = false;
+
+  // The current state of the buttons per function.
+  std::unordered_map<std::string, bool> button_states_;
 };
 } // namespace hector_gamepad_manager
 
--- hector_gamepad_manager_plugins/src/drive_plugin.cpp
+++ hector_gamepad_manager_plugins/src/drive_plugin.cpp
@@ -3,15 +3,10 @@
 namespace hector_gamepad_manager_plugins
 {
 
-DrivePlugin::DrivePlugin()
-  : last_cmd_zero_(false)
-{}
 
-void DrivePlugin::initialize( const rclcpp::Node::SharedPtr &node, const bool active )
+void DrivePlugin::initialize( const rclcpp::Node::SharedPtr &node )
 {
   node_ = node;
-  active_ = active;
-
   plugin_namespace_ = "drive_plugin";
 
   node_->declare_parameters<double>( plugin_namespace_, { { "max_linear_speed", 1.0 },
@@ -31,32 +26,33 @@ void DrivePlugin::initialize( const rclcpp::Node::SharedPtr &node, const bool ac
   fast_factor_ = node_->get_parameter( plugin_namespace_ + ".fast_factor" ).as_double();
 
   drive_command_publisher_ =
-      node_->create_publisher<geometry_msgs::msg::TwistStamped>( "cmd_vel", 10 );
-}
-
-void DrivePlugin::handleButton( const std::string &function, const bool pressed )
-{
-  if ( !active_ ) {
-    return;
-  }
-
-  if ( function == "fast" ) {
-    fast_button_pressed_ = pressed;
-  } else if ( function == "slow" ) {
-    slow_button_pressed_ = pressed;
-  }
+      node_->create_publisher<geometry_msgs::msg::TwistStamped>( "cmd_vel", 1 );
 }
 
 void DrivePlugin::handleAxis( const std::string &function, const double value )
 {
-  if ( !active_ ) {
-    return;
-  }
-
   if ( function == "drive" ) {
     drive_value_ = value;
   } else if ( function == "steer" ) {
     steer_value_ = value;
+  }
+}
+
+void DrivePlugin::handlePress( const std::string &function )
+{
+  if ( function == "fast" ) {
+    fast_mode_active_ = true;
+  } else if ( function == "slow" ) {
+    slow_mode_active_ = true;
+  }
+}
+
+void DrivePlugin::handleRelease( const std::string &function )
+{
+  if ( function == "fast" ) {
+    fast_mode_active_ = false;
+  } else if ( function == "slow" ) {
+    slow_mode_active_ = false;
   }
 }
 
@@ -67,14 +63,14 @@ void DrivePlugin::update()
   }
 
   double speed_factor = normal_factor_;
-  if ( fast_button_pressed_ ) {
-    speed_factor = fast_factor_;
-  } else if ( slow_button_pressed_ ) {
+  if ( slow_mode_active_ ) {
     speed_factor = slow_factor_;
+  } else if ( fast_mode_active_ ) {
+    speed_factor = fast_factor_;
   }
 
-  sendDriveCommand(drive_value_ * max_linear_speed_ * speed_factor,
-    steer_value_ * max_angular_speed_ * speed_factor );
+  sendDriveCommand( drive_value_ * max_linear_speed_ * speed_factor,
+                    steer_value_ * max_angular_speed_ * speed_factor );
 }
 
 void DrivePlugin::activate()
@@ -94,7 +90,8 @@ void DrivePlugin::sendDriveCommand( double linear_speed, double angular_speed )
   drive_command_.header.stamp = node_->now();
   drive_command_.twist.linear.x = linear_speed;
   drive_command_.twist.angular.z = angular_speed;
-  bool current_cmd_zero = drive_command_.twist.linear.x == 0.0 && drive_command_.twist.angular.z == 0.0;
+  bool current_cmd_zero =
+      drive_command_.twist.linear.x == 0.0 && drive_command_.twist.angular.z == 0.0;
   if ( !( current_cmd_zero && last_cmd_zero_ ) ) {
     drive_command_publisher_->publish( drive_command_ );
   }
