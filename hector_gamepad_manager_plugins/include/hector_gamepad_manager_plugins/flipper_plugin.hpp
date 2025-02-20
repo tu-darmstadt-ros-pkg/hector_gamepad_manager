069f49a973b72c273b87220f8cbf1c462c0826a9
md26woxu <marekdaniv@googlemail.com>
2025-02-20 16:30:40 +0100

    initial commit

---
+++ hector_gamepad_manager_plugins/include/hector_gamepad_manager_plugins/flipper_plugin.hpp
@@ -0,0 +1,49 @@
+#ifndef HECTOR_GAMEPAD_MANAGER_PLUGINS_FLIPPER_PLUGIN_HPP
+#define HECTOR_GAMEPAD_MANAGER_PLUGINS_FLIPPER_PLUGIN_HPP
+
+#include <hector_gamepad_manager/gamepad_function_plugin.hpp>
+
+#include "std_msgs/msg/float64_multi_array.hpp"
+
+namespace hector_gamepad_manager_plugins
+{
+class FlipperPlugin : public hector_gamepad_manager::GamepadFunctionPlugin
+{
+public:
+  void initialize( const rclcpp::Node::SharedPtr &node, const bool active ) override;
+
+  void handleButton( const std::string &function, const bool pressed ) override;
+
+  void handleAxis( const std::string &function, const double value ) override;
+
+  void update() override;
+
+  void activate() override;
+
+  void deactivate() override;
+
+private:
+  rclcpp::Node::SharedPtr node_;
+
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr flipper_command_publisher_;
+
+  double speed_;
+  double flipper_front_factor_;
+  double flipper_back_factor_;
+
+  std_msgs::msg::Float64MultiArray flipper_speed_commands_;
+
+  std::vector<double> vel_commands_;
+
+  void set_front_flipper_command(double vel);
+
+  void set_back_flipper_command(double vel);
+
+  void reset_commands();
+
+  void publish_commands();
+
+};
+} // namespace hector_gamepad_manager_plugins
+
+#endif // HECTOR_GAMEPAD_MANAGER_PLUGINS_DRIVE_PLUGIN_HPP
