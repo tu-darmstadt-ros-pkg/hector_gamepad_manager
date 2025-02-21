68d2e147d2e09e5f1f4f96703324c48e6edb4330
Aljoscha Schmidt <aljoscha.schmidt@tu-darmstadt.de>
2025-02-21 09:45:10 +0100

    Integrated new features from main branch

--- hector_gamepad_manager_plugins/src/manipulation_plugin.cpp
+++ hector_gamepad_manager_plugins/src/manipulation_plugin.cpp
@@ -3,10 +3,9 @@
 namespace hector_gamepad_manager_plugins
 {
 
-void ManipulationPlugin::initialize( const rclcpp::Node::SharedPtr &node, const bool active )
+void ManipulationPlugin::initialize( const rclcpp::Node::SharedPtr &node )
 {
   node_ = node;
-  active_ = active;
 
   plugin_namespace_ = "manipulation_plugin";
 
@@ -33,36 +32,43 @@ void ManipulationPlugin::initialize( const rclcpp::Node::SharedPtr &node, const 
   hold_mode_client_ = node_->create_client<std_srvs::srv::SetBool>( "teleop/hold_mode" );
 }
 
-void ManipulationPlugin::handleButton( const std::string &function, const bool pressed )
+void ManipulationPlugin::handlePress( const std::string &function )
 {
-  if ( !active_ ) {
-    return;
+  if ( function == "hold_mode" ) {
+    hold_mode_change_requested_ = true;
+    hold_mode_active_ = true;
   }
-
-  if ( function == "hold_mode" ) { // TODO wait for merge request -> handle on press and release
-    if ( hold_mode_active_ != pressed ) {
-      hold_mode_change_requested_ = true;
-    }
-    hold_mode_active_ = pressed;
-  } else if ( pressed ) { // TODO wait for merge request -> handle on press and release
-    if ( function == "rotate_roll_clockwise" ) {
-      rotate_roll_clockwise = max_eef_angular_speed_;
-    } else if ( function == "rotate_roll_counter_clockwise" ) {
-      rotate_roll_counter_clockwise = max_eef_angular_speed_;
-    } else if ( function == "gripper_open" ) {
-      gripper_cmd_.data = max_gripper_speed_;
-    } else if ( function == "gripper_close" ) {
-      gripper_cmd_.data = max_gripper_speed_;
-    }
+  else if ( function == "rotate_roll_clockwise" ) {
+    rotate_roll_clockwise_ = max_eef_angular_speed_;
+  } else if ( function == "rotate_roll_counter_clockwise" ) {
+    rotate_roll_counter_clockwise_ = -max_eef_angular_speed_;
+  } else if ( function == "gripper_open" ) {
+    open_gripper_ = max_gripper_speed_;
+  } else if ( function == "gripper_close" ) {
+    close_gripper_ = -max_gripper_speed_;
   }
 }
 
+void ManipulationPlugin::handleRelease( const std::string &function )
+{
+  if ( function == "hold_mode" ) {
+    hold_mode_change_requested_ = true;
+    hold_mode_active_ = false;
+  }
+  else if ( function == "rotate_roll_clockwise" ) {
+    rotate_roll_clockwise_ = 0.0;
+  } else if ( function == "rotate_roll_counter_clockwise" ) {
+    rotate_roll_counter_clockwise_ = 0.0;
+  } else if ( function == "gripper_open" ) {
+    open_gripper_ = 0.0;
+  } else if ( function == "gripper_close" ) {
+    close_gripper_ = 0.0;
+  }
+}
+
+
 void ManipulationPlugin::handleAxis( const std::string &function, const double value )
 {
-  if ( !active_ ) {
-    return;
-  }
-
   if ( function == "move_left_right" ) {
     move_left_right_ = value;
   } else if ( function == "move_up_down" ) {
@@ -70,11 +76,11 @@ void ManipulationPlugin::handleAxis( const std::string &function, const double v
   } else if ( function == "move_forward" ) {
     move_forward_ = value;
   } else if ( function == "move_backward" ) {
-    move_backward_ = value;
+    move_backward_ = -value;
   } else if ( function == "rotate_pitch" ) {
-    rotate_pitch = value;
+    rotate_pitch_ = value;
   } else if ( function == "rotate_yaw" ) {
-    rotate_yaw = value;
+    rotate_yaw_ = value;
   }
 }
 
@@ -91,26 +97,26 @@ void ManipulationPlugin::update()
     // reset cmds when hold mode is toggled
     reset();
   }
+  double cmd_vel_linear=0.0, cmd_vel_angular=0.0;
   if ( hold_mode_active_ ) {
     // left joystick moves base, ignore all other axis and buttons
-    drive_cmd_.twist.linear.x = move_up_down_ * max_drive_linear_speed_;
-    drive_cmd_.twist.angular.z = move_left_right_ * max_drive_angular_speed_;
+    cmd_vel_linear = move_up_down_ * max_drive_linear_speed_;
+    cmd_vel_angular = move_left_right_ * max_drive_angular_speed_;
   } else {
-    eef_cmd_.twist.linear.x = ( move_forward_ - move_backward_ ) * max_eef_linear_speed_;
+    eef_cmd_.twist.linear.x = ( move_forward_ + move_backward_ ) * max_eef_linear_speed_;
     eef_cmd_.twist.linear.y = move_left_right_ * max_eef_linear_speed_;
     eef_cmd_.twist.linear.z = move_up_down_ * max_eef_linear_speed_;
     eef_cmd_.twist.angular.x =
-        ( rotate_roll_clockwise - rotate_roll_counter_clockwise ) * max_eef_angular_speed_;
-    eef_cmd_.twist.angular.y = rotate_pitch * max_eef_angular_speed_;
-    eef_cmd_.twist.angular.z = rotate_yaw * max_eef_angular_speed_;
-    gripper_cmd_.data = ( open_gripper - close_gripper ) * max_gripper_speed_;
+        ( rotate_roll_clockwise_ + rotate_roll_counter_clockwise_ ) * max_eef_angular_speed_;
+    eef_cmd_.twist.angular.y = rotate_pitch_ * max_eef_angular_speed_;
+    eef_cmd_.twist.angular.z = rotate_yaw_ * max_eef_angular_speed_;
+    gripper_cmd_.data = ( open_gripper_ + close_gripper_ ) * max_gripper_speed_;
   }
 
   eef_cmd_.header.stamp = node_->now();
   eef_cmd_pub_->publish( eef_cmd_ );
   gripper_cmd_pub_->publish( gripper_cmd_ );
-  drive_cmd_.header.stamp = node_->now();
-  drive_cmd_pub_->publish( drive_cmd_ );
+  sendDriveCommand( cmd_vel_linear, cmd_vel_angular );
 }
 
 void ManipulationPlugin::activate() { active_ = true; }
@@ -123,8 +129,7 @@ void ManipulationPlugin::deactivate()
   eef_cmd_.header.stamp = node_->now();
   eef_cmd_pub_->publish( eef_cmd_ );
   gripper_cmd_pub_->publish( gripper_cmd_ );
-  drive_cmd_.header.stamp = node_->now();
-  drive_cmd_pub_->publish( drive_cmd_ );
+  sendDriveCommand( 0,0 );
   // make sure hold mode is disabled when the plugin is deactivated
   if ( hold_mode_active_ ) {
     const auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
@@ -139,17 +144,31 @@ void ManipulationPlugin::reset()
   move_up_down_ = 0.0;
   move_forward_ = 0.0;
   move_backward_ = 0.0;
-  rotate_pitch = 0.0;
-  rotate_yaw = 0.0;
-  rotate_roll_clockwise = 0.0;
-  rotate_roll_counter_clockwise = 0.0;
-  open_gripper = 0.0;
-  close_gripper = 0.0;
+  rotate_pitch_ = 0.0;
+  rotate_yaw_ = 0.0;
+  rotate_roll_clockwise_ = 0.0;
+  rotate_roll_counter_clockwise_ = 0.0;
+  open_gripper_ = 0.0;
+  close_gripper_ = 0.0;
   eef_cmd_.twist = geometry_msgs::msg::Twist();
   drive_cmd_.twist = geometry_msgs::msg::Twist();
   gripper_cmd_.data = 0.0;
   hold_mode_change_requested_ = false;
 }
+
+void ManipulationPlugin::sendDriveCommand( const double linear_speed, const double angular_speed )
+{
+  drive_cmd_.header.stamp = node_->now();
+  drive_cmd_.twist.linear.x = linear_speed;
+  drive_cmd_.twist.angular.z = angular_speed;
+  const bool current_cmd_zero =
+      drive_cmd_.twist.linear.x == 0.0 && drive_cmd_.twist.angular.z == 0.0;
+  if ( !( current_cmd_zero && last_cmd_zero_ ) ) {
+    drive_cmd_pub_->publish( drive_cmd_ );
+  }
+  last_cmd_zero_ = current_cmd_zero;
+}
+
 } // namespace hector_gamepad_manager_plugins
 
 #include <pluginlib/class_list_macros.hpp>
