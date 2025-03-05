1c05e53c24c0e8eba0bf057bddddb6883d94b50b
Aljoscha Schmidt <aljoscha.schmidt@tu-darmstadt.de>
2025-03-05 17:01:38 +0100

    Load twist_controller

--- hector_gamepad_manager_plugins/src/manipulation_plugin.cpp
+++ hector_gamepad_manager_plugins/src/manipulation_plugin.cpp
@@ -7,14 +7,18 @@ void ManipulationPlugin::initialize( const rclcpp::Node::SharedPtr &node )
 {
   node_ = node;
 
-  std::string plugin_namespace = getPluginName();
+  const auto plugin_namespace = getPluginName();
 
-  node_->declare_parameters<double>( plugin_namespace, { { "max_eef_linear_speed", 1.0 },
-                                                          { "max_eef_angular_speed", 1.0 },
-                                                          { "max_drive_linear_speed", 1.0 },
-                                                          { "max_drive_angular_speed", 1.0 },
-                                                          { "max_gripper_speed", 1.0 } } );
+  node_->declare_parameter<double>( plugin_namespace + ".max_eef_linear_speed", 1.0 );
+  node_->declare_parameter<double>( plugin_namespace + ".max_eef_angular_speed", 1.0 );
+  node_->declare_parameter<double>( plugin_namespace + ".max_drive_linear_speed", 1.0 );
+  node_->declare_parameter<double>( plugin_namespace + ".max_drive_angular_speed", 1.0 );
+  node_->declare_parameter<double>( plugin_namespace + ".max_gripper_speed", 1.0 );
 
+  /*node_->declare_parameter<std::string>( plugin_namespace+ ".twist_controller_name", std::string("moveit_twist_controller") );
+  node_->declare_parameter<std::vector<std::string>>( plugin_namespace + ".stop_controllers",
+                                                       {"arm_trajectory_controller",
+                                                         "gripper_trajectory_controller"} );*/
   max_eef_linear_speed_ =
       node_->get_parameter( plugin_namespace + ".max_eef_linear_speed" ).as_double();
 
@@ -26,10 +30,16 @@ void ManipulationPlugin::initialize( const rclcpp::Node::SharedPtr &node )
       node_->get_parameter( plugin_namespace + ".max_drive_angular_speed" ).as_double();
   max_gripper_speed_ = node_->get_parameter( plugin_namespace + ".max_gripper_speed" ).as_double();
 
+  //twist_controller_name_ = node_->get_parameter( plugin_namespace + ".twist_controller_name" ).as_string();
+  //stop_controllers_ = node_->get_parameter( plugin_namespace + ".stop_controllers" ).as_string_array();
+  twist_controller_name_ = "moveit_twist_controller";
+  stop_controllers_ = {"arm_trajectory_controller", "gripper_trajectory_controller"};
+
   eef_cmd_pub_ = node_->create_publisher<geometry_msgs::msg::TwistStamped>( "teleop/eef_cmd", 10 );
   drive_cmd_pub_ = node_->create_publisher<geometry_msgs::msg::TwistStamped>( "cmd_vel", 10 );
   gripper_cmd_pub_ = node_->create_publisher<std_msgs::msg::Float64>( "teleop/gripper_cmd", 10 );
   hold_mode_client_ = node_->create_client<std_srvs::srv::SetBool>( "teleop/hold_mode" );
+  controller_helper_.initialize( node, plugin_namespace );
 }
 
 std::string ManipulationPlugin::getPluginName() { return "manipulation_plugin"; }
@@ -114,14 +124,26 @@ void ManipulationPlugin::update()
     eef_cmd_.twist.angular.z = rotate_yaw_ * max_eef_angular_speed_;
     gripper_cmd_.data = ( open_gripper_ + close_gripper_ ) * max_gripper_speed_;
   }
+  const bool  is_zero_cmd = isZeroCmd();
+  // make sure controllers are active
+  if (last_eef_cmd_zero_ && !is_zero_cmd) {
+    controller_helper_.switchControllers( { twist_controller_name_ }, stop_controllers_ );
+  }
+  // avoid repeatedly sending zero commands
+  if (!(last_eef_cmd_zero_ && is_zero_cmd)) {
+    eef_cmd_.header.stamp = node_->now();
+    eef_cmd_pub_->publish( eef_cmd_ );
+    gripper_cmd_pub_->publish( gripper_cmd_ );
+  }
+  last_eef_cmd_zero_ = is_zero_cmd;
 
-  eef_cmd_.header.stamp = node_->now();
-  eef_cmd_pub_->publish( eef_cmd_ );
-  gripper_cmd_pub_->publish( gripper_cmd_ );
   sendDriveCommand( cmd_vel_linear, cmd_vel_angular );
 }
 
-void ManipulationPlugin::activate() { active_ = true; }
+void ManipulationPlugin::activate()
+{
+  active_ = true;
+}
 
 void ManipulationPlugin::deactivate()
 {
@@ -156,6 +178,7 @@ void ManipulationPlugin::reset()
   drive_cmd_.twist = geometry_msgs::msg::Twist();
   gripper_cmd_.data = 0.0;
   hold_mode_change_requested_ = false;
+  last_eef_cmd_zero_ = true;
 }
 
 void ManipulationPlugin::sendDriveCommand( const double linear_speed, const double angular_speed )
@@ -165,11 +188,20 @@ void ManipulationPlugin::sendDriveCommand( const double linear_speed, const doub
   drive_cmd_.twist.angular.z = angular_speed;
   const bool current_cmd_zero =
       drive_cmd_.twist.linear.x == 0.0 && drive_cmd_.twist.angular.z == 0.0;
-  if ( !( current_cmd_zero && last_cmd_zero_ ) ) {
+  if ( !( current_cmd_zero && last_drive_cmd_zero_ ) ) {
     drive_cmd_pub_->publish( drive_cmd_ );
   }
-  last_cmd_zero_ = current_cmd_zero;
+  last_drive_cmd_zero_ = current_cmd_zero;
 }
+
+bool ManipulationPlugin::isZeroCmd() const
+{
+  return move_left_right_ == 0.0 && move_up_down_ == 0.0 && move_forward_ == 0.0 &&
+         move_backward_ == 0.0 && rotate_pitch_ == 0.0 && rotate_yaw_ == 0.0 &&
+         rotate_roll_clockwise_ == 0.0 && rotate_roll_counter_clockwise_ == 0.0 &&
+         open_gripper_ == 0.0 && close_gripper_ == 0.0;
+}
+
 
 } // namespace hector_gamepad_manager_plugins
 
